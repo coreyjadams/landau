3a8b7b33bafe5c200bb322baaf9b823ad9737108
Corey Adams <corey.adams@yale.edu>
2016-04-09 15:27:38 -0500

    Fix my stupid exception raising code.

--- landau/__init__.py
+++ landau/__init__.py
@@ -1,9 +1,9 @@
 try:
   import _landau
 except:
-  Raise("Need to build the package first!")
+  raise Exception("Need to build the package first!")
 
 __all__ = ['landau']
 
-
+from landau import gauss, landau, gauss_landau
 
